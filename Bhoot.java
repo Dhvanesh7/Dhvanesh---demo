94a9d181af8ab42ab054621a3d537e3b3ab6804a
Dhvanesh7 <kashiyani.dhvanesh.99@gmail.com>
2024-09-20 21:26:55 +0530

    Add files via upload

---
+++ Bhoot.java
@@ -0,0 +1,26 @@
+class Bhoot 
+{
+		public static void main(String[] args)
+	
+	{ 
+		String name = "Manjulikhha";
+		int age = 205;
+		float hight = 6.1F;
+		double percentage = 95.93;
+		char gender = 'B';
+		long phonenumber = 6565854548L;
+		boolean mstatus = false;
+		String address = "vadaj metro station";
+
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(hight);
+		System.out.println(percentage);
+		System.out.println(gender);
+		System.out.println(phonenumber);
+		System.out.println(mstatus);
+		System.out.println(address);
+	
+	}
+
+}
