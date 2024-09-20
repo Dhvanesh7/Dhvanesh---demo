7c84e305ee5879ee62530caeeb6c91ea7febcf9b
Dhvanesh7 <kashiyani.dhvanesh.99@gmail.com>
2024-09-20 21:28:54 +0530

    Add files via upload

---
+++ Metrostation.java
@@ -0,0 +1,43 @@
+class Metrostation 
+
+{
+		public static void main(String [] args )
+
+	{ 
+
+		String ranip = "ranip metro station";
+		String vadaj = "vadaj metro station";
+		String vijaynagar = "vijay nagar metro station";
+		String ushmanpura = "ushmanpura metro station";
+		String oldhighcourt = "old high court metro station";
+		String running = "old high court metro station to qspider by auto";
+				
+
+		System.out.println(ranip);
+
+		System.out.println(vadaj);
+
+		System.out.println(vijaynagar);
+
+		System.out.println(ushmanpura);
+		
+		System.out.println(oldhighcourt);
+
+		System.out.println(running);
+
+		
+		System.out.println(" 1 First_Metro_Station: "+ranip);
+
+		System.out.println(" 2 Seconds_Metro_Station: "+vadaj);
+
+		System.out.println(" 3 Third_Metro_Station: "+vijaynagar);
+
+		System.out.println(" 4 Forth_Metro_Station: "+ushmanpura);
+		
+		System.out.println(" 5 Fifth_Metro_Station: "+oldhighcourt);
+
+		System.out.println(" 6 Sixth_Metro_Station_To_Qspider_By_Auto: "+running);
+
+}
+
+}
